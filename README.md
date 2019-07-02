88d3b21ebada7e99def437b390d6b28b3a75fc64
Daniele P <daniele.pignedoli@gmail.com>
2019-07-03 00:29:05 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # aframe-camera-controls
-A-frame plugins for Camera-controls
+A-frame plugins for Camera-controls.
+
+# READ CAREFULLY
+This is just a quick'n'dirty (literally nothing more than a copy'n'paste, do not expect nothing classy guys) implementation of [Threejs camera controls Plugin](https://www.npmjs.com/package/camera-controls) as [A-frame](https://aframe.io/) component. The base is the awesome [Superframe](https://github.com/supermedium/superframe) (ex-kframe) [orbit-controls plugin](https://github.com/supermedium/superframe/tree/master/components/orbit-controls)
+
+
+
+# Basic Usage
+
+Add the component to the camera element:
+```
+<a-entity
+  id="the-camera"
+  cameracontrols="initialPosition: 3.5909066036114665 1.5843741492086918 5.575048598071086; dollySpeed: 1; truckSpeed: 2; minPolarAngle: 20; maxPolarAngle: 105; minDistance: 5; maxDistance: 11; dampingFactor: 0.08; target: #the-element-we-are-looking-at;"
+  camera="zoom: 1;"
+  position="0 0 0"
+  rotation="0 0 0">
+</a-entity>
+```
+
+... move the camera as you want, then get the current position:
+
+```
+let position = document.querySelector('#the-camera').components.cameracontrols.cameraControls.toJSON();
+```
+
+Animate the camera to a position:
+```
+document.querySelector('#the-camera').components.cameracontrols.cameraControls.fromJSON( JSON.stringify( position ), true);
+```
+
+Example of a position value:
+```
+{
+  "enabled": true,
+  "minDistance": 0,
+  "maxDistance": 1.7976931348623e+308,
+  "minPolarAngle": 0,
+  "maxPolarAngle": 3.1415926535898,
+  "minAzimuthAngle": -1.7976931348623e+308,
+  "maxAzimuthAngle": 1.7976931348623e+308,
+  "dampingFactor": 0.05,
+  "draggingDampingFactor": 0.25,
+  "dollySpeed": 1,
+  "truckSpeed": 2,
+  "target": [-0.37917888854706, -0.47959704424181, -0.13915770200862],
+  "position": [-4.613750653072203, 4.525158764957159, 2.224967126484331],
+  "target0": [0, 0, 0],
+  "position0": [0, 2, 0]
+}
+```
---
+++ index.js
@@ -0,0 +1,128 @@
+import CameraControls from './lib/CameraControls';
+
+
+var bind = AFRAME.utils.bind;
+
+AFRAME.registerComponent( 'cameracontrols', {
+  dependencies: ['camera'],
+
+  schema: {
+    maxDistance: {default: 1000},
+    minDistance: {default: 1},
+    maxZoom: { default: Infinity },
+    maxPolarAngle: {default: Math.PI },
+    minPolarAngle: {default: 0},
+    minAzimuthAngle: {type: 'number', default: - Infinity},
+    maxAzimuthAngle: {type: 'number', default: Infinity},
+    dampingFactor: {default: 0.05},
+    draggingDampingFactor: {default: 0.25},
+    dollySpeed: {default: 1},
+    truckSpeed: {default: 2},
+
+    target: {type: 'selector'},
+    initialPosition: {type: 'vec3', default: '0 0 0'}
+  },
+
+  init: function () {
+    this.cameraControls = false;
+    this.bindMethods();
+    this.el.addEventListener( 'loaded' , this.installCameraControls );
+  },
+
+
+  installCameraControls: function () {
+    if ( false !== this.cameraControls ) {
+      return;
+    }
+
+    this.bindMethods();
+    var el = this.el;
+    CameraControls.install( { THREE: THREE } );
+
+    var position, positionArray;
+    if ( this.data.target ) {
+      position = this.data.target.getAttribute( 'position' );
+    }
+
+    if ( !position ) {
+      position = { x: 0, y: 2, z: 0 };
+    }
+
+    el.setAttribute( 'position', position );
+    el.getObject3D('camera').position.set( position.x, position.y, position.z );
+
+    this.cameraControls = new CameraControls( el.getObject3D('camera'), el.sceneEl.renderer.domElement );
+
+
+
+    let tmpJSON = Object.assign( {}, this.data );
+
+    tmpJSON.target = [0, 0, 0];
+    tmpJSON.target0 = [0, 0, 0];
+    tmpJSON.enabled = true;
+    tmpJSON.maxPolarAngle = tmpJSON.maxPolarAngle * THREE.Math.DEG2RAD;
+    tmpJSON.minPolarAngle = tmpJSON.minPolarAngle * THREE.Math.DEG2RAD;
+    tmpJSON.position = [tmpJSON.initialPosition.x, tmpJSON.initialPosition.y, tmpJSON.initialPosition.z];
+    tmpJSON.position0 = [position.x, position.y, position.z];
+    delete tmpJSON.initialPosition;
+    //this.cameraControls.fromObj( tmpJSON, true );
+
+
+    /**/
+    this.clock = new THREE.Clock();
+    //this.cameraControls.rotate( -45 * THREE.Math.DEG2RAD, -0.3490658503988659, true );
+  },
+
+
+  onEnterVR: function() {
+    var el = this.el;
+
+    if (!AFRAME.utils.device.checkHeadsetConnected() &&
+        !AFRAME.utils.device.isMobile()) { return; }
+    this.controls.enabled = false;
+    if (el.hasAttribute('look-controls')) {
+      el.setAttribute('look-controls', 'enabled', true);
+      oldPosition.copy(el.getObject3D('camera').position);
+      el.getObject3D('camera').position.set(0, 0, 0);
+    }
+  },
+
+  onExitVR: function() {
+    var el = this.el;
+
+    if (!AFRAME.utils.device.checkHeadsetConnected() &&
+        !AFRAME.utils.device.isMobile()) { return; }
+    this.controls.enabled = true;
+    el.getObject3D('camera').position.copy(oldPosition);
+    if (el.hasAttribute('look-controls')) {
+      el.setAttribute('look-controls', 'enabled', false);
+    }
+  },
+
+
+  bindMethods: function() {
+    this.onEnterVR = bind(this.onEnterVR, this);
+    this.onExitVR = bind(this.onExitVR, this);
+    this.installCameraControls = bind(this.installCameraControls, this);
+  },
+
+  update: function ( oldData ) {
+    if ( this.cameraControls ) {
+      //this.cameraControls.update( this.clock.getDelta() );
+    }
+  },
+
+  tick: function () {
+    if ( this.cameraControls ) {
+      this.cameraControls.update( this.clock.getDelta() );
+    }
+  },
+
+  remove: function() {
+    //this.controls.reset();
+    //this.controls.dispose();
+
+    //this.el.sceneEl.removeEventListener('enter-vr', this.onEnterVR);
+    //this.el.sceneEl.removeEventListener('exit-vr', this.onExitVR);
+  }
+});
